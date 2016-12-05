12f12db82199c6e100b0f184e37ca5ac65c295f8
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-12-05 15:18:59 -0800

    define ui

--- tictactoe.js
+++ tictactoe.js
@@ -2,7 +2,15 @@ const prompt = require('prompt');
 
 var board = [[0,0,0],[0,0,0],[0,0,0]];
 
-function setMark = (posH, posV,mark) => {
+function setMark(posH, posV,mark){
   board[posV][posH] = mark;
 }
 
+function printBoard() {
+  console.log(`Game Board: \n
+  ${board[0][0]} | ${board[0][1]} | ${board[0][0]} \n
+  ${board[1][0]} | ${board[1][1]} | ${board[1][0]} \n 
+  ${board[2][0]} | ${board[2][1]} | ${board[2][0]} \n`)
+}
+
+printBoard();
